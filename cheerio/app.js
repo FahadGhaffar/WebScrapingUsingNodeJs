3428f2cb45377981209d7044d333065a62c417e5
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2023-02-14 09:48:43 -0700

    cheerio implement

--- cheerio/app.js
+++ cheerio/app.js
@@ -1,1 +1,23 @@
 import * as cheerio from 'cheerio';
+
+import request from "request";
+
+// console.log("ok");
+
+request("https://code.org/", (error, response, html) => {
+
+    if (!error && response.statusCode == 200) {
+
+        const $ = cheerio.load(html);
+
+        $("div.header.underscorehover").each((i, el) => {
+            const category = $(el).text();
+            const link = $(el).parent().parent().parent().attr('href');
+
+            console.log(category);
+            console.log((link));
+        })
+
+    }
+
+})
